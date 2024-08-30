61d0652c67b95906716f2df233a7e9cc13d5847f
ℌ𝔞𝔰𝔰𝔞𝔫 ℜ𝔞𝔧𝔭𝔲𝔱 <135378730+HassanRajput0@users.noreply.github.com>
2024-08-31 04:25:49 +0500

    Update main.py

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ def index():
 
     return render_template('index.html', profile_name=profile_name, error_message=error_message)
 
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)s
